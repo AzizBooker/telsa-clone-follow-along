1e684444587956122dc79448216f038b9f5da7c9
AzizBooker <bookeraziz@gmail.com>
2022-01-05 20:42:23 -0500

    Finish Placeholder Model S Screen

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      
       <View style={styles.carContainer}>
-          <View >
-          <Text>Model S</Text>
-          <Text>Starting at $69,420</Text>
+      <ImageBackground 
+      source={require('./assets/images/ModelS.jpeg')}
+      style={styles.image}
+    />
+          <View style={styles.textContainer} >
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69,420</Text>
           </View>
       </View>
+      
+      <StatusBar style="hidden" />
     </View>
   );
 }
@@ -22,6 +29,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   carContainer:{
-
+        width:'100%',
+        height:'100%'
+  },
+  textContainer:{
+      marginTop:'30%',
+      width:'100%',
+      alignItems:"center"
+  },
+  title:{
+    fontSize:40,
+    fontWeight:'500'
+  },
+  subtitle:{
+      fontSize:16
+  },
+  image:{
+    position:'absolute',
+    width:"100%",
+    height:'100%',
+    resizeMode:'cover',
+   
   }
 });
